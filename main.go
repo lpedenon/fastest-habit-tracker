726ef9049393d32bd66391c003e102b0dc660e36
lpedenon <leonpedenon@Leons-MacBook-Pro.local>
2024-09-29 17:30:49 -0400

    initial start to command, just initialized cobra.Command struct and error handling

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-  "fmt"
-  "math"
-  "math/rand"
-  //"strconv"
+  // "fmt"
+  // "math"
+  //  "math/rand"
+  // "strconv"
+  "github.com/spf13/cobra"
+  "os"
 ) 
 
-var c, python, java = true, false, "hello"
-
 func main() {
-  fmt.Println(rand.Int())
-  fmt.Println("hello world")
-  fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
-  g := 3 // to declare use :=
-  g = 4
-  g += 4
-  g++
-  a, b := sum_product(3, 4)
-  fmt.Println(a, b)
-  var i int
-  fmt.Println(i, c, python, java)
-}
+  rootCmd := &cobra.Command{
+    Use: "habit",
+    Short: "Simple habit tracker",
+  }
 
-func sum_product(x, y int) (int, int) {
-  return x + y, x * y
+  if err := rootCmd.Execute(); err != nil {
+        os.Exit(1)
+    }
+  
 }
