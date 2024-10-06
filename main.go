c43ef623eead8890a397391bfb869318d54daa6b
lpedenon <leonpedenon@Leons-MacBook-Pro.local>
2024-10-06 18:00:26 -0400

    put complete command in main.go

--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-  // "fmt"
-  // "math"
-  //  "math/rand"
-  // "strconv"
-  "github.com/spf13/cobra"
-  "os"
-  "github.com/lpedenon/habit/cmd"
-) 
+	// "fmt"
+	// "math"
+	//  "math/rand"
+	// "strconv"
+	"github.com/lpedenon/habit/cmd"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 func main() {
-  rootCmd := &cobra.Command{
-    Use: "habit",
-    Short: "Simple habit tracker",
-  }
+	rootCmd := &cobra.Command{
+		Use:   "habit",
+		Short: "Simple habit tracker",
+	}
 
-  rootCmd.AddCommand(cmd.AddCmd)
-  rootCmd.AddCommand(cmd.ListCmd)
+	rootCmd.AddCommand(cmd.AddCmd)
+	rootCmd.AddCommand(cmd.ListCmd)
+	rootCmd.AddCommand(cmd.CompleteCmd)
 
-  if err := rootCmd.Execute(); err != nil {
-        os.Exit(1)
-    }
-  
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+
 }
